e377d468601994ad53e561543db281676b945754
Josh Robey <joshua.robey@cdicorp.com>
2018-02-13 14:12:56 -0700

    Adding #461 Hamming Distance (Java Solution)

---
+++ 461. Hamming Distance.java
@@ -0,0 +1,42 @@
+/* 
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+ 0 ≤ x, y < 2^31. 
+
+Example: 
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different.
+*/
+
+class Solution {
+    public int hammingDistance(int x, int y) {
+        //Convert ints to 32 bit binary value representations
+        String xBinaryString = String.format("%32s", Integer.toBinaryString(x)).replace(' ','0');
+        String yBinaryString = String.format("%32s", Integer.toBinaryString(y)).replace(' ','0');   
+        
+        //Values for comparisons
+        int hammingCount = 0;
+        char subStringX;
+        char subStringY;
+        
+        for(int i = 0; i < 32; i++) {
+            subStringX = xBinaryString.charAt(i);
+            subStringY = yBinaryString.charAt(i);            
+            if(subStringX != subStringY) {
+                hammingCount++;
+            }
+        }
+        return hammingCount;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# LeetCode_Accepted_Answers
-LeetCode answers I've written and were accepted.
+# Accepted Leetcode Answers
+
+LeetCode answers I've written and were accepted. Includes problem explanation as a comment.
