b6126295173a891812e63a9e6d162d692127f8ba
robeyjj@yahoo.com <robeyjj@yahoo.com>
2021-12-29 10:07:01 -0700

    Fix sentence

--- Easy/26. Remove Duplicated from Sorted Array.php
+++ Easy/26. Remove Duplicated from Sorted Array.php
@@ -7,6 +7,6 @@ class Solution
      */
     function removeDuplicates(&$nums) 
     {
-        $nums = array_unique($nums); //This is cheating, and not the point of the exercise, but why build a space ship with a bicycle will do?
+        $nums = array_unique($nums); //This is cheating, and not the point of the exercise, but why build a space ship when a bicycle will do?
     }
 }
